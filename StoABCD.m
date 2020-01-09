5a9dec9093953158a08f96342051dd767a922d3c
Florian <florianrenneke@gmail.com>
2020-01-09 22:41:54 +0100

    Update StoABCD.m

--- StoABCD.m
+++ StoABCD.m
@@ -1,18 +1,17 @@
 %
 % Convert 2 Port Scattering Parameter Matrix to ABCD Matrix
-%
+% (Source: http://www2.electron.frba.utn.edu.ar/~jcecconi/Bibliografia/04%20-%20Param_S_y_VNA/Parameters%20Conversion%20Table.pdf)
 % V1 = A*V2 - B*I2
 % I1 = C*V2 - D*I2
 
 function ABCD = StoABCD (S, Z01, Z02)
 
-ABCD = zeros(2, 2);
-ABCD(1,1) = ((conj(Z01)+Z01*S(1,1))*(1-S(2,2)) + S(1,2)*S(2,1)*Z01) ./ (2*S(2,1)*sqrt(real(Z01) * real(Z02)));
-ABCD(1,2) = ((conj(Z01)+Z01*S(1,1))*(conj(Z02)+S(2,2)*Z02)-S(1,2)*S(2,1)*Z01*Z02)./(2*S(2,1)*sqrt(real(Z01) * real(Z02)));
-ABCD(2,1) = ((1-S(1,1))*(1-S(2,2))-S(1,2)*S(2,1))./(2*S(2,1)*sqrt(real(Z01) * real(Z02)));
-ABCD(2,2) = ((1-S(1,1))*(conj(Z02)+S(2,2)*Z02)+S(1,2)*S(2,1)*Z02)/(2*S(2,1)*sqrt(real(Z01) * real(Z02)));
+  ABCD = zeros(2, 2);
+  ABCD(1,1) = ((conj(Z01)+Z01*S(1,1))*(1-S(2,2)) + S(1,2)*S(2,1)*Z01) ./ (2*S(2,1)*sqrt(real(Z01) * real(Z02)));
+  ABCD(1,2) = ((conj(Z01)+Z01*S(1,1))*(conj(Z02)+S(2,2)*Z02)-S(1,2)*S(2,1)*Z01*Z02)./(2*S(2,1)*sqrt(real(Z01) * real(Z02)));
+  ABCD(2,1) = ((1-S(1,1))*(1-S(2,2))-S(1,2)*S(2,1))./(2*S(2,1)*sqrt(real(Z01) * real(Z02)));
+  ABCD(2,2) = ((1-S(1,1))*(conj(Z02)+S(2,2)*Z02)+S(1,2)*S(2,1)*Z02)/(2*S(2,1)*sqrt(real(Z01) * real(Z02)));
 
-
-endfunction
+end
 
 
